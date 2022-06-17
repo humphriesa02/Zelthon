a03ac3851e682c7897926c6788aba8eb4be3347a
Hero <humphriesa02@gmail.com>
2022-06-16 23:08:20 -0700

    Random stuff

--- 02 - setup/2 - setup/code/main.py
+++ 02 - setup/2 - setup/code/main.py
@@ -1,26 +1,29 @@
-import pygame, sys
+import pygame
+import sys
+
 from settings import *
 
 class Game:
-	def __init__(self):
-		  
-		# general setup
-		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Zelda')
-		self.clock = pygame.time.Clock()
-	
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
+    def __init__(self):
 
-			self.screen.fill('black')
-			pygame.display.update()
-			self.clock.tick(FPS)
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('Zelda')
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
 
 if __name__ == '__main__':
-	game = Game()
-	game.run()	
+    game = Game()
+    game.run()
